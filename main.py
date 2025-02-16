5305fc3a53648d1e49bda2152943f9683a120643
tanishqsujan <tanishq.kothamasu@gmail.com>
2025-02-16 10:43:09 +0400

    login

--- main.py
+++ main.py
@@ -2,70 +2,87 @@ from flask import Flask, render_template, request
 import mysql.connector
 import re
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username= request.form['username']
-        password= request.form['password']
-        mydb= mysql.connector.connect(host="db4free.net",
-                                      user="rootcodingal_123",
-                                      password="root1234",
-                                      database="students123")
-        mycursor= mydb.cursor()
+        username = request.form['username']
+        password = request.form['password']
+        mydb = mysql.connector.connect(host="http://localhost/phpmyadmin/",
+                                       user="wXy63pU5g1",
+                                       password="qVOgRNMGRK",
+                                       database="wXy63pU5g1")
+        mycursor = mydb.cursor()
         mycursor.execute(
-            'SELECT * FROM users WHERE username = %s AND password = %s',
+            'SELECT * FROM LoginDetails WHERE username = %s AND password = %s',
             (username, password))
-        account= mycursor.fetchone()
+        account = mycursor.fetchone()
         if account:
             print('login success')
-            name= account[1]
-            id= account[0]
-            msg= "Logged in Successfully"
-            print('login successful')
-            render_template('index.html', msg=msg, name=name, id=id)
+            name = account[1]
+            id = account[0]
+            msg = 'Logged in Successfully'
+            print('login successfull')
+            return render_template('index.html', msg=msg, name=name, id=id)
         else:
-            msg= "Incorrect credentials. Kindly check"
+            msg = 'incorrect Credentials. Kindly check'
             return render_template('login.html', msg=msg)
     else:
-        return render_template("login.html")
-    
-    
+        return render_template('login.html')
+
+
 @app.route('/logout')
 def logout():
-    name= ''
-    id= ''
-    msg= "Logged out Successfully"
+    name = ''
+    id = ''
+    msg = 'Logged out succesfully'
     return render_template('login.html', msg=msg, name=name, id=id)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        username= request.form['username']
-        password= request.form['password']
-        email= request.form['email']
-        mydb= mysql.connector.connect(host="db4free.net",
-                                      user="rootcodingal_123",
-                                      password="root1234",
-                                      database="students123")
-        mycursor= mydb.cursor()
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        mydb = mysql.connector.connect(host="remotemysql.com",
+                                       user="wXy63pU5g1",
+                                       password="qVOgRNMGRK",
+                                       database="wXy63pU5g1")
+        mycursor = mydb.cursor()
         print(username)
         mycursor.execute(
-            'SELECT * FROM users WHERE username = %s AND password = %s',
-            (username, password))
-        account= mycursor.fetchone()
+            'SELECT * FROM LoginDetails WHERE username = %s AND email_id = %s',
+            (username, email))
+        account = mycursor.fetchone()
         print(account)
         if account:
-            msg= "Account already exists!"
+            msg = 'Account already exists !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg= "Invalid email address!"
+            msg = 'Invalid email address !'
         elif not re.match(r'[A-Za-z0-9]+', username):
-            msg= 'Username must carry only characters and numbers!'
+            msg = 'Username must contain only characters and numbers !'
         elif not username or not password or not email:
-            msg= "Kindly fill in the Details!"
+            msg = 'Kindly fill the details !'
         else:
-            mycursor.execute('INSER INTO LoginDetails VALUES ( %s, %s, %s)', (username, email, password))
+            mycursor.execute('INSERT INTO LoginDetails VALUES ( %s, %s, %s)',
+                             (username, email, password))
             mydb.commit()
+            msg = 'Your Registration is Successful'
+            name = username
+            return render_template('index.html', msg=msg, name=name)
+    elif request.method == 'POST':
+        msg = 'Kindly fill the details !'
+    return render_template('registeration.html', msg=msg)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('login.html')
+
+
+app.run(host='0.0.0.0', port=8080)
